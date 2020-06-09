56b027a3d8467804aa7c0edd8bad3c8adaf504dc
Salilthip Phuklang <ung_1002@hotmail.com>
2020-06-09 20:42:12 +0700

    fix port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
     const express = require('express')
     app = express()
     bodyParser = require('body-parser')
-    port = process.env.PORT || 6000
+    port = process.env.PORT || 5000
 
     app.listen(port);
 
